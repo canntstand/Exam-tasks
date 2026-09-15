3df72d08a9443aaacc74ef8d97e2a43be723b9b2
Roman Frolov <r9888582703@gmail.com>
2026-09-15 21:39:44 +0300

    14.3 solved

---
+++ 14 task/3.py
@@ -0,0 +1,19 @@
+def count_0(n):
+    cnt = 0
+    
+    while n > 0:
+        if n % 7 == 0:
+            cnt += 1
+        n //= 7
+    
+    return cnt
+
+ans = 0
+
+for x in range(1, 2031):
+    eq = 7 ** 170 + 7 ** 100 - x
+    hmz = count_0(eq)
+    if hmz == 71 and x > ans:
+        ans = x
+    
+print(ans)
